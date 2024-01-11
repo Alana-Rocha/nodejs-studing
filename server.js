83454476b0ca8c3ea1f0e2fefa47d9a030da5b80
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2024-01-11 13:23:37 -0300

    teste

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-import {createServer} from 'node:http'
+//NodeJS
+
+// import { createServer } from "node:http";
+
+// const server = createServer((request, response) => {
+//   response.write("Teste");
+
+//   return response.end();
+// });
+
+// server.listen(3333);
+
+//Fastify
+
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.post("/api/:id", (req) => {
+  return { teste: req.params.id };
+});
+
+server.listen({ port: 3333 });
